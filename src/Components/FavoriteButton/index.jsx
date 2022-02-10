2d7290346d1a859d9643f6e675b023c7538c1b32
Maxi <maximiliano.tortosa@tagle.net.ar>
2022-02-10 16:27:37 -0300

    Ecommerce Cards4

--- src/Components/FavoriteButton/index.jsx
+++ src/Components/FavoriteButton/index.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import styled from 'styled-components';
 import { MdOutlineFavoriteBorder, MdOutlineFavorite } from 'react-icons/md';
 
 const FavoriteButton = () => {
